5048c81752234a7f14f82383f9f7ff341354646f
omustafa1994 <omustafa1994@gmail.com>
2018-12-03 14:24:37 +0000

    added bird

--- js/main.js
+++ js/main.js
@@ -1,58 +1,75 @@
-//retrieve canvas
-var canvas = document.getElementById('canvas');
-context = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', function () {
+  //after you run live-server you will need to refresh the page one time. 
 
-//run functions
-drawBackground();
-drawPipes();
+  //retrieve canvas
+  var canvas = document.getElementById('canvas');
+  context = canvas.getContext('2d');
 
-//*********************************************************************************************************************/
-// Background properties
-//*********************************************************************************************************************/
+  //run functions
+  drawBackground();
+  drawPipes();
+  drawForeground();
+  drawBird();
 
-function drawBackground() {
-  //create and draw background image 
-  backgroundImage = new Image();
-  backgroundImage.src = 'assets/background.png';
-  backgroundImage.onload = function () {
-    context.drawImage(backgroundImage, 0, 0);
+  //*********************************************************************************************************************
+  // Background properties
+  //*********************************************************************************************************************
+
+  function drawBackground() {
+    //create and draw background image 
+    backgroundImage = new Image();
+    backgroundImage.src = 'assets/background.png';
+    backgroundImage.onload = function () {
+      context.drawImage(backgroundImage, 0, 0);
+    }
   }
-}
 
-//*********************************************************************************************************************/
-// Foreground properties
-//*********************************************************************************************************************/
+  //*********************************************************************************************************************
+  // Foreground properties
+  //*********************************************************************************************************************
 
-const foregroundClass = drawForeground();
-console.log(foregroundClass);
-
-function drawForeground() {
-  //create and draw foreground image 
-  foregroundImage = new Image();
-  foregroundImage.src = 'assets/foreground.png';
-  foregroundImage.onload = function () {
-    context.drawImage(foregroundImage, 0, 400);
+  function drawForeground() {
+    //create and draw foreground image 
+    foregroundImage = new Image();
+    foregroundImage.src = 'assets/foreground.png';
+    foregroundImage.onload = function () {
+      context.drawImage(foregroundImage, 0, 400);
+    }
   }
-}
 
-//*********************************************************************************************************************/
-// Pipes properties
-//*********************************************************************************************************************/
+  //*********************************************************************************************************************
+  // Pipes properties
+  //*********************************************************************************************************************
 
-//variables
-var pipeGap = 100; //set distance between North and South pipes
-var constant = pipeNorthImage.height + pipeGap; //the North pipe & pipe gap will determine where South pipe begins
+  //variables
+  var pipeGap = 100; //set distance between North and South pipes
+  var constant = pipeNorthImage.height + pipeGap; //the North pipe & pipe gap will determine where South pipe begins
 
-function drawPipes() {
-  //create and draw pipe images
-  pipeNorthImage = new Image(); //north pipe
-  pipeNorthImage.src = 'assets/pipeNorth.png';
-  pipeNorthImage.onload = function () {
-    context.drawImage(pipeNorthImage, 100, 0); //x and y axis
+  function drawPipes() {
+    //create and draw pipe images
+    pipeNorthImage = new Image(); //north pipe
+    pipeNorthImage.src = 'assets/pipeNorth.png';
+    pipeNorthImage.onload = function () {
+      context.drawImage(pipeNorthImage, 100, 0); //x and y axis
+    }
+    pipeSouthImage = new Image(); //south pipe
+    pipeSouthImage.src = 'assets/pipeSouth.png';
+    pipeSouthImage.onload = function () {
+      context.drawImage(pipeSouthImage, 100, 0 + constant); //x and y axis (y axis is determined by variable 'constant')
+    }
   }
-  pipeSouthImage = new Image(); // south pipe
-  pipeSouthImage.src = 'assets/pipeSouth.png';
-  pipeSouthImage.onload = function () {
-    context.drawImage(pipeSouthImage, 100, 0 + constant); //x and y axis (y axis is determined by variable 'constant')
+
+  //*********************************************************************************************************************
+  // Background properties
+  //*********************************************************************************************************************
+
+  function drawBird() {
+    //create and draw background image 
+    birdImage = new Image();
+    birdImage.src = 'assets/bird.png';
+    birdImage.onload = function () {
+      context.drawImage(birdImage, 0, 0);
+    }
   }
-}
+
+});
