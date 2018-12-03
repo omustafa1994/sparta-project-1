8f2ae42d2e63e4636b8b9417f6ce4535d529b412
omustafa1994 <omustafa1994@gmail.com>
2018-12-03 11:59:42 +0000

    priority setting

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 var canvas = document.getElementById('canvas');
 context = canvas.getContext('2d');
 
-//variables
-
 //run functions
 drawBackground();
 drawPipes();
@@ -11,6 +9,7 @@ drawPipes();
 //*********************************************************************************************************************/
 // Background properties
 //*********************************************************************************************************************/
+
 function drawBackground() {
   //create and draw background image 
   backgroundImage = new Image();
@@ -21,18 +20,39 @@ function drawBackground() {
 }
 
 //*********************************************************************************************************************/
+// Foreground properties
+//*********************************************************************************************************************/
+
+const foregroundClass = drawForeground();
+console.log(foregroundClass);
+
+function drawForeground() {
+  //create and draw foreground image 
+  foregroundImage = new Image();
+  foregroundImage.src = 'assets/foreground.png';
+  foregroundImage.onload = function () {
+    context.drawImage(foregroundImage, 0, 400);
+  }
+}
+
+//*********************************************************************************************************************/
 // Pipes properties
 //*********************************************************************************************************************/
+
+//variables
+var pipeGap = 100; //set distance between North and South pipes
+var constant = pipeNorthImage.height + pipeGap; //the North pipe & pipe gap will determine where South pipe begins
+
 function drawPipes() {
   //create and draw pipe images
-  pipeNorthImage = new Image();
+  pipeNorthImage = new Image(); //north pipe
   pipeNorthImage.src = 'assets/pipeNorth.png';
   pipeNorthImage.onload = function () {
-    context.drawImage(pipeNorthImage, 110, 0); //x and y axis
+    context.drawImage(pipeNorthImage, 100, 0); //x and y axis
   }
-  pipeSouthImage = new Image();
+  pipeSouthImage = new Image(); // south pipe
   pipeSouthImage.src = 'assets/pipeSouth.png';
   pipeSouthImage.onload = function () {
-    context.drawImage(pipeSouthImage, 0, 0); //x and y axis
+    context.drawImage(pipeSouthImage, 100, 0 + constant); //x and y axis (y axis is determined by variable 'constant')
   }
 }
