8d63f6e67b9fd722f6d63e0966aca5594fdb58f7
omustafa1994 <omustafa1994@gmail.com>
2018-12-05 10:53:01 +0000

    new version

--- js/main.js
+++ js/main.js
@@ -48,9 +48,9 @@ document.addEventListener('DOMContentLoaded', function () {
       constant = pipeNorth.height + pipeGap; //determines height of South pipe
       context.drawImage(pipeNorth, pipe[i].x, pipe[i].y); //x and y axis of North pipe
       context.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant); //x and y axis of South pipe
-      pipe[i].x--; //decrement the x axis of the generated pipes  
+      pipe[i].x--; //decrement the x axis of the generated pipes (moving effect)
 
-      if (pipe[i].x == 115) { //creates distance between generated pipes 
+      if (pipe[i].x == 115) { //creates distance between the generated pipes 
         pipe.push({
           x: canvas.width,
           y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height //randomly generate y axis of North pipe 
@@ -58,22 +58,22 @@ document.addEventListener('DOMContentLoaded', function () {
       }
 
       //collision detection
-      if (birdAxisX + bird.width >= pipe[i].x && birdAxisX <= pipe[i].x + pipeNorth.width && (birdAxisY <= pipe[i].y + pipeNorth.height || birdAxisY + bird.height >= pipe[i].y + constant) || birdAxisY + bird.height >= canvas.height - foreground.height) { // compares x and y axis of the bird and the pipes to check for any overlap
+      if (birdAxisX + bird.width >= pipe[i].x && birdAxisX <= pipe[i].x + pipeNorth.width && (birdAxisY <= pipe[i].y + pipeNorth.height || birdAxisY + bird.height >= pipe[i].y + constant) || birdAxisY + bird.height >= canvas.height - foreground.height) { // compares x and y axis of the bird and the pipes/the foreground to check for any overlap
         location.reload(); // reload the page if there is an overlap
       }
 
       //scoreboard
-      if (pipe[i].x == 5) { //checks if pipe x axis passes the x axis of the bird (bird being on x axis of 5)
-        pointSound.play(); //play sound 
+      if (pipe[i].x == 10) { //checks if pipe x axis passes the x axis of the bird (bird being on x axis of 10)
+        pointSound.play(); //play MP3 sound 
         score++; //increment score by 1 
       }
     }
-    context.drawImage(foreground, 0, canvas.height - foreground.height); //y axis places foreground 
+    context.drawImage(foreground, 0, canvas.height - foreground.height); //y axis places foreground at bottom of canvas
     context.drawImage(bird, birdAxisX, birdAxisY); //x and y axis is determined by bird variables
     birdAxisY += gravity; //adds gravity to y axis of bird
 
     context.font = "25px Verdana"; //text font style
-    context.fillText("Score : " + score, 10, 30); //x and y axis of text plus score
+    context.fillText("Score : " + score, 10, 30); //x and y axis of text and score
 
     requestAnimationFrame(drawAll); //executes code on the next available screen repaint
   }
@@ -90,7 +90,7 @@ document.addEventListener('DOMContentLoaded', function () {
   //bird variables
   var birdAxisX = 10; //set x axis
   var birdAxisY = 150; //set y axis
-  var gravity = 1; //set gravity
+  var gravity = 0.25; //set gravity
 
   var score = 0; //set score
 
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // var flySound = new Audio();
   var pointSound = new Audio();
-  // var scoreSound = new Audio();
+  // var deathSound = new Audio();
   pointSound.src = 'audio/points.mp3';
 
   //*********************************************************************************************************************
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function checkKeyPressed(e) { //keycodes: left = 37 || up = 38 || right = 39 || down = 40
     if (e.keyCode == "38") { //if key code for UP-Arrow is pressed then move y axis of bird
-      birdAxisY -= 25;
+      birdAxisY -= 25; //makes bird go up (and not down) on y axis
     }
   }
 
