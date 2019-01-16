7af44bd80cf12f107bb7d8d05ffeb2804cb10c5e
omustafa1994 <omustafa1994@gmail.com>
2019-01-16 00:52:46 +0000

    a bunch of fixes

--- javascript/main.js
+++ javascript/main.js
@@ -8,29 +8,25 @@ context = canvas.getContext('2d');
 
 //create background image 
 background = new Image();
-background.src = 'assets/background.png'; //source path
+background.src = '../assets/background.png'; //source path
 
 //create foreground image 
 foreground = new Image();
-foreground.src = 'assets/foreground.png';
+foreground.src = '../assets/foreground.png';
 
 //create bird image 
 bird1 = new Image();
-bird1.src = 'assets/bird1.png';
-// bird2 = new Image();
-// bird2.src = 'assets/bird2.png';
-// bird3 = new Image();
-// bird3.src = 'assets/bird3.png';
-// bird4 = new Image();
-// bird4.src = 'assets/bird4.png';
+bird1.src = '../assets/bird1.png';
+bird2 = new Image();
+bird2.src = '../assets/bird2.png';
+bird3 = new Image();
+bird3.src = '../assets/bird3.png';
+bird4 = new Image();
+bird4.src = '../assets/bird4.png';
 
 //create start image
 start = new Image();
-start.src = 'assets/start.png';
-
-//create instruction image
-tap = new Image();
-tap.src = 'assets/tap.png';
+start.src = '../assets/start.png';
 
 //*********************************************************************************************************************
 // Game variables
@@ -38,7 +34,27 @@ tap.src = 'assets/tap.png';
 
 //foreground variables
 var foregroundAxisX1 = 0;
-var foregroundAxisX2 = 288; // canvas width 
+var foregroundAxisX2 = 288; //canvas width
+
+//timer for bird animation
+dateBefore = Date.now()
+
+function animate() {
+  dateAfter = Date.now();
+  dateNow = dateAfter - dateBefore
+  
+  if (dateNow > 300) { //every 100 miliseconds the bird changes image
+    context.drawImage(bird1, 10, 150);
+    if (dateNow > 400) //creates delay of extra 100 milliseconds for smoother bird animation loop
+    dateBefore = dateAfter;
+  } else if (dateNow > 200) { //every 100 miliseconds the bird changes image
+    context.drawImage(bird2, 10, 150);
+  } else if (dateNow > 100) { //every 100 miliseconds the bird changes image
+    context.drawImage(bird3, 10, 150);
+  } else { //every 100 miliseconds the bird changes image
+    context.drawImage(bird2, 10, 150);
+  }
+};
 
 //*********************************************************************************************************************
 // Draw images to canvas
@@ -58,10 +74,14 @@ function drawAll() {
   }
   
   context.drawImage(start, 50, 20); //x and y axis 
-  context.drawImage(tap, 83, 170); //x and y axis 
+
+  context.font = "25px Verdana"; //text font style
+  context.fillText("Click anywhere", 40, 470); //x and y axis of text and score
+  context.font = "10px Verdana"; //text font style
+  context.fillText("{ Sound warning }", 80, 490); //x and y axis of text and score
   
-  // animate(); run bird flap animation
+  animate(); //run bird flap animation
   
   requestAnimationFrame(drawAll); //executes code on the next available screen repaint
-  }
-  drawAll(); //runs all functions in a set order
+};
+drawAll(); //runs all functions in a set order
